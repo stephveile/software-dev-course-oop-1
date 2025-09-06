1c679601f976b47fcad1c5b810d3f1e098ba4bc3
stephveile <veile.steph@gmail.com>
2025-09-06 10:42:56 -0500

    Initial commit

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -41,4 +41,6 @@ public class Student {
 
     // Add a void method called addAbsence that increments the daysAbsent field by 1.
     // Add a void method called addBonusPoints that takes a double parameter bonusPoints and adds it to the grade field.
+    public void addAbsence() { this.daysAbsent += 1; }
+    public void addBonusPoints(double bonusPoints) { this.grade = grade + bonusPoints; }
 }
